71a6898ea3100a7b8e435f8e3e7e6eb7a1ad7755
Iván Uribe <nem.jiuribe@coppel.com>
2026-04-14 11:50:32 -0600

    Finalización de práctica 01 del módulo 04 donde se secuencian prompts

--- Bloque 4_Cadenas/01-Cadena Secuencial Simple.py
+++ Bloque 4_Cadenas/01-Cadena Secuencial Simple.py
@@ -17,3 +17,19 @@ chain = LLMChain(llm=chat, prompt=chat_prompt_template)
 print(chain)
 
 print(chain.invoke(input="Lavadoras"))
+
+#  Cadena Secuencia Simple
+from langchain_classic.chains.sequential import SimpleSequentialChain
+llm = ChatOllama(model="gpt-oss:20b")
+template = "Dame un simple resumen con un listado de puntos para un post de un blog acerca de {tema}"
+prompt1 = ChatPromptTemplate.from_template(template)
+chain_1 = LLMChain(llm=llm, prompt=prompt1)
+
+template = "Escribe un post completo usando este resumen: {resumen}"
+prompt2 = ChatPromptTemplate.from_template(template)
+chain_2 = LLMChain(llm=llm, prompt=prompt2)
+
+full_chain = SimpleSequentialChain(chains=[chain_1, chain_2], verbose=True) #verbose=True nos irá dando paso a paso lo que hace, pudiendo ver los resultados intermedios
+
+result = full_chain.invoke(input="Inteligencia Artificial")
+print(result)
